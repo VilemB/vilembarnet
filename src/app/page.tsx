0242d465bf14ff68a8e1daf48ed90552a4abb716
VilemB <barnetv7@gmail.com>
2026-01-19 11:14:55 +0100

    saves

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 
+import * as THREE from "three";
 import { useRef } from "react";
 
 export default function Home() {
 
   const textContainerRef = useRef<HTMLDivElement>(null);
-  
   let easeFactor = 0.02;
-  
-  let scene, camera, renderer, planeMesh;
-
-  let mousePosition = { x: 0.5, y: 0.5};
-  let targetMousePosition = { x: 0.5, y: 0.5};
-  let prevMousePosition = { x: 0.5, y: 0.5};
+  let scene: THREE.Scene, camera: THREE.Camera, renderer: THREE.WebGLRenderer, planeMesh: THREE.Mesh;
+  let mousePosition = { x: 0.5, y: 0.5 };
+  let targetMousePosition = { x: 0.5, y: 0.5 };
+  let prevPosition = { x: 0.5, y: 0.5 };
 
   const vertexShader = `
     varying vec2 vUv;
@@ -24,33 +22,204 @@ export default function Home() {
 
   const fragmentShader = `
     varying vec2 vUv;
-    uniform sampler2D uTexture;
+    uniform sampler2D u_texture;
     uniform vec2 u_mouse;
     uniform vec2 u_prevMouse;
 
     void main() {
       vec2 gridUV = floor(vUv * vec2(40.0, 40.0)) / vec2(40.0, 40.0);
       vec2 centerOfPixel = gridUV + vec2(1.0/40.0, 1.0/40.0);
-      
+
       vec2 mouseDirection = u_mouse - u_prevMouse;
-      
+
       vec2 pixelToMouseDirection = centerOfPixel - u_mouse;
       float pixelDistanceToMouse = length(pixelToMouseDirection);
       float strength = smoothstep(0.3, 0.0, pixelDistanceToMouse);
-      
-      vec2 uvOffset = strength * -mouseDirection * 0.3;
+
+      vec2 uvOffset = strength * -mouseDirection * 0.4;
       vec2 uv = vUv - uvOffset;
 
-      vec4 color = texture2D(uTexture, uv);
+      vec4 color = texture2D(u_texture, uv);
       gl_FragColor = color;
     }
   `;
+
+  function createTextTexture(text: string, font: string, size: number, color: string, fontWeight = "100") {
+    const canvas = document.createElement("canvas");
+    const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+
+    const canvasWidth = window.innerWidth * 2;
+    const canvasHeight = window.innerHeight * 2;
+
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    if (!ctx) {
+      return;
+    }
+
+    ctx.fillStyle = color || "#F3F0F0";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const fontSize = size || Math.floor(canvasWidth * 2);
+
+    ctx.fillStyle = "#001F3D";
+    ctx.font = `${fontWeight} ${fontSize}px "${font || "Italiana"}"`;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+
+    ctx.imageSmoothingEnabled = true;
+    ctx.imageSmoothingQuality = "high";
+
+    const textMetrics = ctx.measureText(text);
+    const textWidth = textMetrics.width;
+
+    const scaleFactor = Math.min(1, (canvasWidth * 1) / textWidth);
+    const aspectCorrection = canvasWidth / canvasHeight;
+
+    ctx.setTransform(
+      scaleFactor,
+      0,
+      0,
+      scaleFactor / aspectCorrection,
+      canvasWidth / 2,
+      canvasHeight / 2
+    );
+
+    ctx.strokeStyle = "#ED985F";
+    ctx.lineWidth = fontSize * 0.005;
+    for (let i = 0; i < 3; i++) {
+      ctx.strokeText(text, 0, 0);
+    }
+    ctx.fillText(text, 0, 0);
+
+    return new THREE.CanvasTexture(canvas);
+  }
+
+  function initializeScene(texture: THREE.Texture) {
+    scene = new THREE.Scene();
+
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    camera = new THREE.OrthographicCamera(
+      -1,
+      1,
+      1 / aspectRatio,
+      -1 / aspectRatio,
+      0.1,
+      1000
+    );
+    camera.position.z = 1;
+
+    let shaderUniforms = {
+      u_mouse: { type: "v2", value: new THREE.Vector2() },
+      u_prevMouse: { type: "v2", value: new THREE.Vector2() },
+      u_texture: { type: "t", value: texture },
+    };
+
+    planeMesh = new THREE.Mesh(
+      new THREE.PlaneGeometry(2, 2),
+      new THREE.ShaderMaterial({
+        uniforms: shaderUniforms,
+        vertexShader,
+        fragmentShader,
+      })
+    );
+
+    scene.add(planeMesh);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setClearColor(0xffffff, 1);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    textContainerRef.current?.appendChild(renderer.domElement);
+  }
+
+  function reloadTexture() {
+    const newTexture = createTextTexture(
+      "vilem barnet",
+      "Italiana",
+      100,
+      "#ED985F",
+      "400"
+    );
+    planeMesh.material.uniforms.u_texture.value = newTexture;
+  }
+
+  initializeScene(
+    createTextTexture("vilem barnet", "Italiana", 100, "#001F3D", "400")
+  );
+
+  function animateScene() {
+    requestAnimationFrame(animateScene);
+
+    mousePosition.x += (targetMousePosition.x - mousePosition.x) * easeFactor;
+    mousePosition.y += (targetMousePosition.y - mousePosition.y) * easeFactor;
+
+    planeMesh.material.uniforms.u_mouse.value.set(
+      mousePosition.x,
+      1.0 - mousePosition.y
+    );
+
+    planeMesh.material.uniforms.u_prevMouse.value.set(
+      prevPosition.x,
+      1.0 - prevPosition.y
+    );
+
+    renderer.render(scene, camera);
+  }
+
+  animateScene();
+
+  textContainerRef.current?.addEventListener("mousemove", handleMouseMove);
+  textContainerRef.current?.addEventListener("mouseenter", handleMouseEnter);
+  textContainerRef.current?.addEventListener("mouseleave", handleMouseLeave);
+
+  function handleMouseMove(event: MouseEvent) {
+    easeFactor = 0.035;
+    let rect = textContainerRef.current?.getBoundingClientRect();
+    prevPosition = { ...targetMousePosition };
+    if (!rect) return;
+
+    targetMousePosition.x = (event.clientX - rect.left) / rect.width;
+    targetMousePosition.y = (event.clientY - rect.top) / rect.height;
+  }
+
+  function handleMouseEnter(event: MouseEvent) {
+    easeFactor = 0.01;
+    let rect = textContainerRef.current?.getBoundingClientRect();
+    if (!rect) return;
+
+    mousePosition.x = targetMousePosition.x =
+      (event.clientX - rect.left) / rect.width;
+    mousePosition.y = targetMousePosition.y =
+      (event.clientY - rect.top) / rect.height;
+  }
+
+  function handleMouseLeave() {
+    easeFactor = 0.01;
+    targetMousePosition = { ...prevPosition };
+  }
+
+  window.addEventListener("resize", onWindowResize, false);
+
+  function onWindowResize() {
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    camera.left = -1;
+    camera.right = 1;
+    camera.top = 1 / aspectRatio;
+    camera.bottom = -1 / aspectRatio;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    reloadTexture();
+  }
 
   return (
     <main className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <div ref={textContainerRef} id="textContainer">
-          <h1 className="text-6xl mb-4">vilem barnet</h1>
         </div>
         <p className="text-xl text-dark/70">
           Your Next.js project is ready to build.
