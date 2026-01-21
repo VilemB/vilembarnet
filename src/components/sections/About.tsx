a05b8653b2a93a3e05bb6a1792f7366f000a3eac
VilemB <barnetv7@gmail.com>
2026-01-21 18:36:00 +0100

    animation

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,7 +1,52 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+
 export default function About() {
-  return (
-    <section className="about">
-      <h2>I enjoy designing and building digital products <span className="italic font-bold">from scratch.</span></h2>
-    </section>
-  );
+    const aboutRef = useRef<HTMLDivElement>(null);
+    const firstLineRef = useRef<HTMLDivElement>(null);
+    const secondLineRef = useRef<HTMLDivElement>(null);
+
+    gsap.registerPlugin(ScrollTrigger);
+    useGSAP(() => {
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: aboutRef.current,
+                start: "bottom bottom",
+                end: "+=400",
+                scrub: true,
+                pin: true,
+                markers: true,
+            }
+        });
+
+        tl.to(firstLineRef.current, {
+            opacity: 0,
+            y: -30,
+            duration: .4,
+            ease: "power3.out",
+        })
+        .fromTo(secondLineRef.current, {
+            opacity: 0,
+            y: 30,
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: .4,
+            ease: "power3.out",
+        })
+        
+
+    })
+
+    return (
+        <section ref={aboutRef} className="about">
+            <h2 className="" ref={firstLineRef}>I enjoy designing and building digital products</h2>
+            <h2 className="" ref={secondLineRef}>Here are some of the projects I've worked on</h2>
+        </section>
+    );
 }
