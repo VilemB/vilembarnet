a9e044166812a869c43fb052b452345d66a65f92
VilemB <barnetv7@gmail.com>
2026-01-24 21:35:24 +0100

    photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,39 @@ import Image from "next/image";
 
 export default function Home() {
   return (<section className="min-h-screen px-6 md:px-12 lg:px-16">
-    <div className="grid grid-cols-12 gap-5 items-center">
+    <div className="grid grid-cols-12 gap-5">
 
       <div className="col-span-12">
-        <div className="flex flex-col leading-none">
-
-          <h1 className="text-[clamp(4rem,18vw,18rem)] font-extralight">
-            Vilém
-          </h1>
-
-          <h1 className="text-[clamp(4rem,18vw,18rem)] font-extralight -mt-[0.15em] text-right">
-            Barnet
-          </h1>
-
-        </div>
+        <h1 className="text-[clamp(4rem,18vw,18rem)] font-extralight leading-none">
+          Vilém
+        </h1>
       </div>
 
-      <div className="col-span-3 col-start-2 ">
+      <div className="col-span-1 col-start-1 self-end">
+        <Image
+          src="/logos/logo.svg"
+          alt="Hero"
+          width={50}
+          height={50}
+          className="w-1/2 h-auto"
+        />
+      </div>
+
+      <div className="col-span-3 col-start-2 self-end">
         <Image
           src="/website/vilem2.webp"
           alt="Hero"
           width={500}
           height={500}
           className="w-full h-auto"
+          style={{ filter: "grayscale(100%)" }}
         />
+      </div>
+
+      <div className="col-span-8 self-start">
+        <h1 className="text-[clamp(4rem,20vw,18rem)] font-extralight leading-none text-right m-0">
+          Barnet
+        </h1>
       </div>
 
     </div>
