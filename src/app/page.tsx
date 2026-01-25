ce161afffb41d437aeea49a836eb83c9550c251a
VilemB <barnetv7@gmail.com>
2026-01-25 15:22:17 +0100

    sketch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,18 @@ export default function Home() {
     <>
       <section className="flex flex-col items-center justify-center h-screen">
 
-        <div className="">
+        <div className="flex-1 flex-row items-center justify-center">
           <p>
             Design should evoke emotion and feel natural. Through thoughtful design and development, I focus on creating digital experiences that are clear and intentional.
           </p>
+          <div>
+            
+          </div>
         </div>
         <div className="pixelated-text">
           <PixelatedText className="absolute bottom-0">
-            barnetvilem
+            @barnetvilem
           </PixelatedText>
-        </div>
-
-        <div className="bg-dark h-[20svh] w-full rounded-t-2xl">
-
         </div>
 
       </section>
