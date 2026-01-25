37b5484c6527868785103a25d665241491b51eed
VilemB <barnetv7@gmail.com>
2026-01-25 15:35:55 +0100

    responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,22 @@ import PixelatedText from "@/components/PixelatedText";
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col h-screen p-8 md:p-16 gap-4">
-        <div className="flex flex-row flex-1 gap-4">
-          <div className="flex-1 flex flex-col justify-start">
-            <h2 className="text-xl md:text-2xl font-medium mb-6">
-              web development <span className="mx-2">•</span> design
+      <section className="home">
+        <div className="home-top-row">
+          <div className="home-content-cell">
+            <h2 className="home-heading">
+              web development <span className="home-bullet">•</span> design
             </h2>
-            <p className="text-base md:text-lg leading-relaxed max-w-lg">
+            <p className="home-description">
               Design should evoke emotion and feel natural. Through thoughtful design and development, I focus on creating digital experiences that are clear and intentional.
             </p>
           </div>
 
-          <div className="flex-1">
+          <div className="home-empty-cell">
           </div>
         </div>
 
-        <div className="flex-1 flex items-end justify-center">
+        <div className="home-bottom-row">
           <div className="pixelated-text">
             <PixelatedText>
               @barnetvilem
