970b4a015772dcc1605a56202d3a61f1b039a2ca
VilemB <barnetv7@gmail.com>
2026-01-26 14:20:00 +0100

    CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/sections/Hero";
 import About from "@/components/sections/About";
+import CTA from "@/components/sections/CTA";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <CTA />
     </>
   );
 }
---
+++ src/components/sections/CTA.tsx
@@ -0,0 +1,24 @@
+import { ArrowRight } from "lucide-react";
+
+export default function CTA() {
+    return (
+        <section className="cta">
+            <div className="cta-container">
+                
+                <div className="cta-item">
+                    <h2>EXPLORE MY WORK</h2>
+                </div>
+                <div className="cta-item">
+                    <h2>GET TO KNOW ME</h2>
+                </div>
+                <div className="cta-item">
+                    <h2>READ MY BLOG</h2>
+                </div>
+                <div className="cta-item">
+                    <h2>GET IN TOUCH</h2>
+                </div>
+                
+            </div>
+        </section>
+    );
+}
