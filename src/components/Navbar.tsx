25bb86f7659c0d557d4b7d77d42a51bb9594f44a
VilemB <barnetv7@gmail.com>
2026-01-27 19:21:21 +0100

    text hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
+import { textHoverAnimation } from "@/utils/gsapAnimations";
 
 const Navbar = () => {
+    const linksRef = useRef<(HTMLAnchorElement | null)[]>([]);
+
+    useEffect(() => {
+        linksRef.current.forEach((link) => {
+            if (link) {
+                textHoverAnimation(link);
+            }
+        });
+    }, []);
+
     return (
         <nav className="navbar">
             <div className="navbar-container">
@@ -15,10 +29,18 @@ const Navbar = () => {
                     />
                 </Link>
                 <div className="navbar-links">
-                    <Link href="/work">work</Link>
-                    <Link href="/about">about</Link>
-                    <Link href="/blog">blog</Link>
-                    <Link href="mailto:barnetv7@gmail.com">contact</Link>
+                    <Link href="/work" ref={(el) => { linksRef.current[0] = el; }}>
+                        work
+                    </Link>
+                    <Link href="/about" ref={(el) => { linksRef.current[1] = el; }}>
+                        about
+                    </Link>
+                    <Link href="/blog" ref={(el) => { linksRef.current[2] = el; }}>
+                        blog
+                    </Link>
+                    <Link href="mailto:barnetv7@gmail.com" ref={(el) => { linksRef.current[3] = el; }}>
+                        contact
+                    </Link>
                 </div>
             </div>
         </nav>
